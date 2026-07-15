47c13befeec4f100b1c8512e385bc2fd51f9c3ad
geeta sathe <145338743+Geetasathe123@users.noreply.github.com>
2026-07-15 11:05:52 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+// Simple Java program to demonstrate compilation and execution.
+public class HelloWorld {
+
+    // Main method - program execution starts here.
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Java program compiled and executed successfully.");
+    }
+}
