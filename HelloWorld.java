71b37cafa8c9ec6a40d6938fd60bb34dc26052f8
geeta sathe <145338743+Geetasathe123@users.noreply.github.com>
2026-07-15 10:57:43 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+// This is the HelloWorld class.
+public class HelloWorld {
+
+    // The main method is the entry point of the Java program.
+    public static void main(String[] args) {
+
+        // Prints "Hello World" to the console.
+        System.out.println("Hello World");
+
+        // Prints an additional message to the console.
+        System.out.println("Welcome to Java Programming!");
+    }
+}
